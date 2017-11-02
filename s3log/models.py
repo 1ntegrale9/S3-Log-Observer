983fb33b94de5eb4ec858c38fff1cab0ef24bc87
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-11-02 15:02:45 +0900

    implement create records

--- s3log/models.py
+++ s3log/models.py
@@ -1,41 +1,63 @@
 from django.db import models
 
 class S3AccessLog(models.Model):
-    bucket_owner = models.CharField(max_length=1024)
-    bucket_name = models.CharField(max_length=1024)
+    bucket_owner     = models.CharField(max_length=1024)
+    bucket_name      = models.CharField(max_length=1024)
     request_datetime = models.DateTimeField()
-    remote_ip = models.CharField(max_length=1024)
-    requesta = models.CharField(max_length=1024)
-    request_id = models.CharField(max_length=1024)
-    operation = models.CharField(max_length=1024)
-    request_key = models.CharField(max_length=1024)
-    request_uri = models.CharField(max_length=1024)
-    http_status = models.IntegerField()
-    error_code = models.CharField(max_length=1024)
-    bytes_sent = models.IntegerField()
-    object_size = models.IntegerField()
-    total_time = models.IntegerField()
+    remote_ip        = models.CharField(max_length=1024)
+    requesta         = models.CharField(max_length=1024)
+    request_id       = models.CharField(max_length=1024)
+    operation        = models.CharField(max_length=1024)
+    request_key      = models.CharField(max_length=1024)
+    request_uri      = models.CharField(max_length=1024)
+    http_status      = models.IntegerField()
+    error_code       = models.CharField(max_length=1024)
+    bytes_sent       = models.IntegerField()
+    object_size      = models.IntegerField()
+    total_time       = models.IntegerField()
     turn_around_time = models.IntegerField()
-    referrer = models.CharField(max_length=1024)
-    user_agent = models.CharField(max_length=1024)
-    version_id = models.CharField(max_length=1024)
+    referrer         = models.CharField(max_length=1024)
+    user_agent       = models.CharField(max_length=1024)
+    version_id       = models.CharField(max_length=1024)
 
 class S3AccessLog2(models.Model):
-    bucket_owner = models.CharField(max_length=1024)
-    bucket_name = models.CharField(max_length=1024)
+    bucket_owner     = models.CharField(max_length=1024)
+    bucket_name      = models.CharField(max_length=1024)
     request_datetime = models.CharField(max_length=1024)
-    remote_ip = models.CharField(max_length=1024)
-    requesta = models.CharField(max_length=1024)
-    request_id = models.CharField(max_length=1024)
-    operation = models.CharField(max_length=1024)
-    request_key = models.CharField(max_length=1024)
-    request_uri = models.CharField(max_length=1024)
-    http_status = models.CharField(max_length=1024)
-    error_code = models.CharField(max_length=1024)
-    bytes_sent = models.CharField(max_length=1024)
-    object_size = models.CharField(max_length=1024)
-    total_time = models.CharField(max_length=1024)
+    remote_ip        = models.CharField(max_length=1024)
+    requesta         = models.CharField(max_length=1024)
+    request_id       = models.CharField(max_length=1024)
+    operation        = models.CharField(max_length=1024)
+    request_key      = models.CharField(max_length=1024)
+    request_uri      = models.CharField(max_length=1024)
+    http_status      = models.CharField(max_length=1024)
+    error_code       = models.CharField(max_length=1024)
+    bytes_sent       = models.CharField(max_length=1024)
+    object_size      = models.CharField(max_length=1024)
+    total_time       = models.CharField(max_length=1024)
     turn_around_time = models.CharField(max_length=1024)
-    referrer = models.CharField(max_length=1024)
-    user_agent = models.CharField(max_length=1024)
-    version_id = models.CharField(max_length=1024)
+    referrer         = models.CharField(max_length=1024)
+    user_agent       = models.CharField(max_length=1024)
+    version_id       = models.CharField(max_length=1024)
+
+def CreateRecord(dataset):
+    S3AccessLog2.objects.create(
+        bucket_owner     = dataset['bucket_owner'],
+        bucket_name      = dataset['bucket_name'],
+        request_datetime = dataset['request_datetime'],
+        remote_ip        = dataset['remote_ip'],
+        requesta         = dataset['requesta'],
+        request_id       = dataset['request_id'],
+        operation        = dataset['operation'],
+        request_key      = dataset['request_key'],
+        request_uri      = dataset['request_uri'],
+        http_status      = dataset['http_status'],
+        error_code       = dataset['error_code'],
+        bytes_sent       = dataset['bytes_sent'],
+        object_size      = dataset['object_size'],
+        total_time       = dataset['total_time'],
+        turn_around_time = dataset['turn_around_time'],
+        referrer         = dataset['referrer'],
+        user_agent       = dataset['user_agent'],
+        version_id       = dataset['version_id'],
+    )
--- s3log/views.py
+++ s3log/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+# coding:utf-8
 
-# Create your views here.
+from __future__ import print_function
+from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django import forms
+from .models import CreateRecord
+import os
+import sys
+import locale
+import codecs
+import traceback
+import json
+
+def IndexView(request):
+    LogOutput('log/')
+    return render(request,'s3log/index.html')
+
+def SimpleCharDet(b_str):
+    def TryDecode(b_str, c_code):
+        try:
+            b_str.decode(c_code)
+            return c_code
+        except UnicodeDecodeError:
+            return 'UDError'
+    c_code_list = ['us-ascii', 'utf-8', 'cp932', 'eucjp']
+    for c_code in c_code_list:
+        if TryDecode(b_str, c_code) != c_code_list: return c_code
+    return 'unknown'
+
+def GetCoding(filepath):
+    locale.setlocale(locale.LC_ALL, '')
+    maxsize = 1 * 1024 * 1024
+    def SysExit(e, errorMsg):
+        sys.exit("Error: {4} '{0}' [{1}]: {2}".format(filepath, e.errno, e.strerror, errorMsg))
+    def TryRead(f_in):
+        try:
+            return f_in.read(maxsize)
+        except IOError as e:
+            SysExit(e, 'cannot read from file')
+    def TryOpen():
+        try:
+            with open(filepath, 'rb') as f_in: return TryRead(f_in)
+        except IOError as e:
+            SysExit(e, 'cannot open file')
+    return SimpleCharDet(TryOpen())
+
+def FileInput(filepath):
+    with codecs.open(filepath, mode = 'r', encoding = GetCoding(filepath)) as file: return(file.read())
+
+def LogNormalize(logfile):
+    log_list = []
+    for one_str in logfile.splitlines():
+        log_split, log_elem, paren_flag, quote_flag = [], "", False, False
+        for one_char in list(one_str):
+            if one_char == ' ':
+                if not paren_flag and not quote_flag:
+                    log_split.append(log_elem)
+                    log_elem = ""
+                else: log_elem += one_char
+            elif one_char == '[': paren_flag = True
+            elif one_char == ']': paren_flag = False
+            elif one_char == '"': quote_flag = not quote_flag
+            else: log_elem += one_char
+        if log_elem != "":
+            log_split.append(log_elem)
+        json = dict(zip(S3Keys(), log_split))
+        log_list.append(json)
+    return log_list
+
+def LogOutput(dirpath):
+    for filename in os.listdir(dirpath):
+        try:
+            logfile = FileInput(dirpath + filename)
+            for s3_log in LogNormalize(logfile):
+                CreateRecord(s3_log)
+                print(json.dumps(s3_log, sort_keys=True, indent=4))
+        except:
+            traceback.print_exc()
+
+def S3Keys():
+    return ['bucket_owner'
+           ,'bucket_name'
+           ,'request_datetime'
+           ,'remote_ip'
+           ,'requesta'
+           ,'request_id'
+           ,'operation'
+           ,'request_key'
+           ,'request_uri'
+           ,'http_status'
+           ,'error_code'
+           ,'bytes_sent'
+           ,'object_size'
+           ,'total_time'
+           ,'turn_around_time'
+           ,'referrer'
+           ,'user_agent'
+           ,'version_id']
