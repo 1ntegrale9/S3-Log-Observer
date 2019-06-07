381813feb7409825cc92143b3ada51bc3e581afb
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-06-07 22:04:46 +0900

    Refactor

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,5 +1,3 @@
-# coding:utf-8
-
 from __future__ import print_function
 from django.shortcuts import render
 from .models import CreateRecord
@@ -8,20 +6,23 @@ import os
 import codecs
 import traceback
 
+
 def IndexView(request):
-    LogOutput('s3log/')
-    return render(request,'analyzer/index.html')
+    output('s3log/')
+    return render(request, 'analyzer/index.html')
 
-def FileInput(filepath):
-    with codecs.open(filepath, mode = 'r', encoding = 'us-ascii') as file:
-      try:
-        return(file.read())
-      except:
-        traceback.print_exc()
-        print(filepath)
-        return ""
 
-def LogNormalize(logfile):
+def input(filepath):
+    with codecs.open(filepath, mode='r', encoding='us-ascii') as file:
+        try:
+            return(file.read())
+        except Exception:
+            traceback.print_exc()
+            print(filepath)
+            return ""
+
+
+def parse(logfile):
     log_list = []
     for one_str in logfile.splitlines():
         log_split, log_elem, paren_flag, quote_flag = [], "", False, False
@@ -30,14 +31,25 @@ def LogNormalize(logfile):
                 if not paren_flag and not quote_flag:
                     log_split.append(log_elem)
                     log_elem = ""
-                else: log_elem += one_char
-            elif one_char == '[': paren_flag = True
-            elif one_char == ']': paren_flag = False
-            elif one_char == '"': quote_flag = not quote_flag
-            else: log_elem += one_char
-        if log_elem != "": log_split.append(log_elem)
+                else:
+                    log_elem += one_char
+            elif one_char == '[':
+                paren_flag = True
+            elif one_char == ']':
+                paren_flag = False
+            elif one_char == '"':
+                quote_flag = not quote_flag
+            else:
+                log_elem += one_char
+        if log_elem != "":
+            log_split.append(log_elem)
         log_list.append(log_split)
     return log_list
 
-def LogOutput(dirpath):
-    [[CreateRecord(s3_log) for s3_log in tqdm(LogNormalize(FileInput(dirpath + filename)))] for filename in tqdm(os.listdir(dirpath))]
+
+def output(dirpath):
+    for filename in tqdm(os.listdir(dirpath)):
+        log = input(dirpath + filename)
+        logs = parse(log)
+        for s3log in tqdm(logs):
+            CreateRecord(s3log)
