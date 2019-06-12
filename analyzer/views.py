da2fc9dcc7e89910a5ecea2615b0f37d4e3a1738
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-06-12 10:52:31 +0900

    Refactor

--- analyzer/views.py
+++ analyzer/views.py
@@ -22,30 +22,30 @@ def input(filepath):
         return ""
 
 
-def parse(logfile, callback):
-    for one_str in logfile.splitlines():
-        log_split, log_elem, paren_flag, quote_flag = [], "", False, False
-        for one_char in list(one_str):
-            if one_char == ' ':
-                if not paren_flag and not quote_flag:
-                    log_split.append(log_elem)
-                    log_elem = ""
-                else:
-                    log_elem += one_char
-            elif one_char == '[':
-                paren_flag = True
-            elif one_char == ']':
-                paren_flag = False
-            elif one_char == '"':
-                quote_flag = not quote_flag
+def parse(log, callback):
+    log_split, log_elem, paren_flag, quote_flag = [], "", False, False
+    for one_char in list(log):
+        if one_char == ' ':
+            if not paren_flag and not quote_flag:
+                log_split.append(log_elem)
+                log_elem = ""
             else:
                 log_elem += one_char
-        if log_elem != "":
-            log_split.append(log_elem)
-        callback(log_split)
+        elif one_char == '[':
+            paren_flag = True
+        elif one_char == ']':
+            paren_flag = False
+        elif one_char == '"':
+            quote_flag = not quote_flag
+        else:
+            log_elem += one_char
+    if log_elem != "":
+        log_split.append(log_elem)
+    callback(log_split)
 
 
 def output(dirpath):
     for filename in tqdm(os.listdir(dirpath)):
         log = input(dirpath + filename)
-        logs = parse(log, CreateRecord)
+        for one_str in logfile.splitlines():
+            parse(log, CreateRecord)
